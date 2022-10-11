eb63eb5fef2ef6ca7d4d349fea04b35e6ccd43c9
veruzhskaya <veruzhskaya@gmail.com>
2022-10-11 14:25:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data, dataTwo } from './data';
 import './App.css';
 
+
+
+
 function App() {
+  const [seasons, setSeasons] = useState (data)
+ 
+  const [pictures, setPictures] = useState(0)
+  
+  const {picture} = dataTwo[pictures]
+  const [showMore, setShowMore] = useState (false) 
+
+
+  const nextPicture = () => {
+    setPictures ((pictures => {
+    pictures ++;
+  if (pictures> dataTwo.length-1) {
+    pictures=0;
+   }
+   return pictures
+}))
+   }
+
+   const previousPicture =() => {
+    setPictures ((pictures => {
+      pictures --;
+      if (pictures<0) {
+        return dataTwo.length-1;
+      }
+      return pictures
+    }))
+  }
+
+  const removeSeason= (id) => {
+    let newSeasons = seasons.filter (season =>
+      season.id !==id
+    )
+    setSeasons (newSeasons)
+
+  
+
+    
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   
+ <div className='container'>
+  <h1><span>fashion 2022-2023.</span> {seasons.length} seasons!</h1>
+ </div>
+ <div className='container'>
+<h3>{showMore? "What is worth trying on in the fall and winter of 2023 to be in trend? Fashion trends this time are extremely diverse, designers were willingly inspired by the fashion of the 80s, 90s and 2000s, and which of their ideas to adopt is up to you.":"What is worth trying on in the fall and winter of 2023 to be in trend? Fashion trends this time are extremely diverse, designers were willingly inspired by the fashion of the 80s, 90s and 2000s, and which of their ideas to adopt is up to you.".substring(0,100)+ "..."}
+<button className='show' onClick={() => setShowMore(!showMore)}>{showMore? "show less": "show more"}</button></h3>
+ </div>
+ <div className='container'>
+  <button className='next' onClick={previousPicture}>previous</button>
+  <img className='picture' src={picture} alt="people"/>
+  <button className='next' onClick={nextPicture}>next</button>
+ </div>
+
+ {seasons.map (( element => {
+  const {id, season, image} = element
+  return (
+<div key={id}>
+  <div className='container'>
+  <h2>{id}. {season}</h2>
+  </div>
+  <div className='container'>
+  <img className='photo' src={image} alt='fashion'/>
+  </div>
+
+  <div className='container'>
+  <button className='remove' onClick={() => removeSeason(id)}>remove</button>
+  </div>
+</div>
+  )
+ }))}
+ <div className='container'>
+  <button className='btn' onClick={() => setSeasons([])}>delete all</button>
+ </div>
     </div>
   );
+ 
+
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const data = [
+{
+    id: 1,
+    season: "Winter",
+    image: "https://images.unsplash.com/photo-1542542856-d29decffd6ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZnVyJTIwY29hdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    
+ },
+
+{
+    id: 2,
+    season: "Spring",
+    image: "https://images.unsplash.com/photo-1557776198-8b17010cbbb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3ByaW5nJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    id: 3,
+    season: "Summer",
+    image: "https://images.unsplash.com/photo-1542295669297-4d352b042bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c3VtbWVyJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+{
+    id: 4,
+    season: "autumn",
+    image: "https://images.unsplash.com/photo-1578632292335-df3abbb0d586?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXV0dW1uJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+},
+
+
+]
+
+export const dataTwo =
+[ 
+{
+  picture: "https://images.unsplash.com/photo-1581044777550-4cfa60707c03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+ 
+},
+{
+  picture: "https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+ 
+},
+{
+    picture: "https://images.unsplash.com/photo-1610918019759-a6873cc84e72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZ1ciUyMGNvYXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    
+  },
+  {
+    picture: "https://images.unsplash.com/photo-1596993100471-c3905dafa78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzN8fGZhc2hpb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    
+  },
+  
+]
+
